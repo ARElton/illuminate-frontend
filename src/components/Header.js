4fde577ce07307b8701c7a112740c1251b3fb62f
Amodttech <aaron@amodttech.com>
2021-01-22 15:55:12 -0500

    header structure built

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-import PatternForm from './PatternForm';
 import Search from './Search';
 import Categories from './Categories';
 import Login from './Login';
 
 function Header() {
 
+
+    return (
+        <header>
+        <h1>Illuminate</h1>
+        <nav>
+            <NavLink exact to="/" className="button">
+            Home
+            </NavLink>
+            <NavLink to="/projects/categories" className="button">
+            Categories
+            </NavLink>
+            <NavLink to="/patterns/new" className="button">
+            Add Pattern
+            </NavLink>
+            <NavLink to="/login" className="button">
+            Login
+            </NavLink>
+        </nav>
+        <Search />
+        </header>
+    )
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom'
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import PatternView from './components/PatternView';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
 
 ReactDOM.render(
-  <Router>
-    <Route exact path="/">
-      <App />
-    </Route>
-    <Route exact path="/patterns/:id">
-      <PatternView />
-    </Route>
-    <Route exact path="/patterns/new">
-      <PatternForm />
-    </Route>
-    <Route exact path="/login">
-      <Login />
-    </Route>
-  </Router>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
